63b5ff162b1f0df3ffc0c49051e416a28389b8da
SuHadzh <32958558+SuHadzh@users.noreply.github.com>
2017-11-10 14:39:25 +0200

    Add solutions

---
+++ homework01/FastFib.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+	long int fibNum = 1, a = 1, b = 1;
+
+	for (int i = 2; i < num; i++)
+	{
+		fibNum = a + b;
+		a = b;
+		b = fibNum;
+	}
+
+	cout << fibNum << endl;
+
+	return 0;
+}
---
+++ homework01/RecursiveFib.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+long int recursiveFib(int num)
+{
+	if (num == 1 || num == 2)
+		return 1;
+	return recursiveFib(num - 1) + recursiveFib(num - 2);
+}
+
+int main()
+{
+	int number;
+	cin >> number;
+
+	cout << recursiveFib(number) << endl;
+
+	return 0;
+}
